fca77fc044993c9ac673adebaeb2f64362f290ab
Shaniya Clement <shaniyaclement@fb.com>
2022-07-20 17:20:35 -0700

    Installed and added permissions for Google Play SDK in AndroidManifest and build.gradle. Connected the location services in the FeedFragment, which now asks the user for location permissions (if not already allowed) when they open the feed fragment. Uses a looper that request location updates while user uses the application.

--- app/src/main/java/com/example/journeyjournal/fragments/FeedFragment.java
+++ app/src/main/java/com/example/journeyjournal/fragments/FeedFragment.java
@@ -1,18 +1,24 @@
 package com.example.journeyjournal.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,12 +27,17 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.journeyjournal.Activities.ComposePostActivity;
-import com.example.journeyjournal.Activities.RemindersActivity;
 import com.example.journeyjournal.ParseConnectorFiles.Post;
 import com.example.journeyjournal.Adapters.PostsAdapter;
 import com.example.journeyjournal.Activities.LoginActivity;
-import com.example.journeyjournal.ParseConnectorFiles.Reminder;
 import com.example.journeyjournal.R;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.parse.DeleteCallback;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
@@ -35,6 +46,7 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("deprecation")
 public class FeedFragment extends Fragment {
     private static final String TAG = "FeedFragment";
     private SwipeRefreshLayout swipeContainer;
@@ -44,21 +56,19 @@ public class FeedFragment extends Fragment {
     protected PostsAdapter adapter;
     protected List<Post> allPosts;
 
+    double longitude;
+    double latitude;
+
     public FeedFragment() {
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        ConnectivityManager connManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if(wifi.isConnected()){
-            // query posts from the database
-            Log.i(TAG, "onResume");
-            adapter.clear();
-            queryPosts();
-        } else {
-            querySavedPosts();}
+        // query posts from the database
+        Log.i(TAG, "onResume");
+        adapter.clear();
+        querySavedPosts();
     }
 
     @Override
@@ -70,7 +80,8 @@ public class FeedFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ConnectivityManager connManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        startLocationUpdates();
+        ConnectivityManager connManager = (ConnectivityManager) requireActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
         ibLogout = view.findViewById(R.id.ibLogout);
@@ -83,29 +94,18 @@ public class FeedFragment extends Fragment {
         rvPosts.setAdapter(adapter);
         // set the layout manager on the recycler view
         rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
-        if(wifi.isConnected()){
-            // query posts from the database
-            Log.i(TAG, "onResume");
-            adapter.clear();
-            queryPosts();
-        } else {
-            querySavedPosts();}
+        querySavedPosts();
 
         swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
         // Setup refresh listener which triggers new data loading
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                // Your code to refresh the list here.
-                // Make sure you call swipeContainer.setRefreshing(false)
-                // once the network request has completed successfully.
-                if(wifi.isConnected()){
-                    // query posts from the database
-                    Log.i(TAG, "onResume");
-                    adapter.clear();
-                    queryPosts();
-                } else {
-                    querySavedPosts();}
+        swipeContainer.setOnRefreshListener(() -> {
+            // Your code to refresh the list here.
+            // Make sure you call swipeContainer.setRefreshing(false)
+            // once the network request has completed successfully.
+            if (wifi.isConnected()) {
+                queryPosts();
+            } else {
+                querySavedPosts();
             }
         });
         // Configure the refreshing colors
@@ -118,10 +118,9 @@ public class FeedFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 ParseUser.logOut();
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                Intent i = new Intent(getContext(), LoginActivity.class);
-                startActivity(i);
-                getActivity().finish();
+                Intent intent = new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                requireActivity().finish();
             }
         });
 
@@ -133,17 +132,68 @@ public class FeedFragment extends Fragment {
         });
     }
 
-    private void goNewPost() {
-        ConnectivityManager connManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if(wifi.isConnected()){
-            Intent intent = new Intent(getActivity(), ComposePostActivity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(getContext(), "Please connect to the internet", Toast.LENGTH_LONG).show();
+    // Trigger new location updates at interval
+    protected void startLocationUpdates() {
+
+        // Create the location request to start receiving updates
+        LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        /* 10 secs */
+        long UPDATE_INTERVAL = 10 * 1000;
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        /* 2 sec */
+        long FASTEST_INTERVAL = 2000;
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(requireActivity());
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                    new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                    100);
+            return;
         }
+        LocationServices.getFusedLocationProviderClient(requireActivity()).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        // do work here
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+        Toast.makeText(getActivity(), "Permission ok", Toast.LENGTH_SHORT).show();
+
     }
 
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        String msg = "Updated Location: " +
+                latitude + "," +
+                longitude;
+        Toast.makeText(getActivity(), "on location changed", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+    }
+
+
+    private void goNewPost() {
+        Intent intent = new Intent(getActivity(), ComposePostActivity.class);
+        startActivity(intent);
+    }
 
     private void queryPosts() {
         // specify what type of data we want to query - Post.class
@@ -153,11 +203,23 @@ public class FeedFragment extends Fragment {
         query.setLimit(20);
         query.setSkip(0);
         // order posts by creation date (newest first)
-        query.addDescendingOrder("createdAt");
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
         // start an asynchronous call for posts
         query.findInBackground(new FindCallback<Post>() {
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             public void done(List<Post> posts, ParseException e) {
+
+                Post.unpinAllInBackground(posts);
+
+                // Remove the previously cached results.
+                Post.unpinAllInBackground("Posts", new DeleteCallback() {
+                    public void done(ParseException e) {
+                        // Cache the new results.
+                        Post.pinAllInBackground("Posts", posts);
+                    }
+                });
+
                 if (e != null) {
                     Log.e(TAG, "Issue with getting posts", e);
                     return;
@@ -174,29 +236,33 @@ public class FeedFragment extends Fragment {
         });
     }
 
-    private void querySavedPosts(){
+    protected void querySavedPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
-        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
         query.fromLocalDatastore();
-        query.addDescendingOrder("createdAt");
         query.findInBackground(new FindCallback<Post>() {
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             public void done(List<Post> posts, ParseException e) {
-                // check for failure
+                Log.i(TAG, posts.toString());
+
                 if (e != null) {
-                    Log.e(TAG, "Failure to load saved reminders", e);
+                    Log.e(TAG, "Issue getting posts.", e);
                     return;
                 }
-                // prints every reminder description for debugging purposes
-                for (Post post : posts){
+
+                // at this point, we have gotten the posts successfully
+                for (Post post : posts) {
                     Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
                 }
-                // save received comments to list and notify adapter of change
+
                 allPosts.clear();
                 allPosts.addAll(posts);
                 adapter.notifyDataSetChanged();
                 swipeContainer.setRefreshing(false);
-            }});
+            }
+        });
     }
 }
+
